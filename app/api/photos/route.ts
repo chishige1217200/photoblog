bd16927a406dea6ac0a5315dd925f63ba47a1254
chishige1217200 <daichicchi01@gmail.com>
2026-02-10 02:54:08 +0900

    Update: API処理内容の整理

--- app/api/photos/route.ts
+++ app/api/photos/route.ts
@@ -3,15 +3,12 @@ import { auth } from "@/auth";
 export async function GET() {
   const session = await auth();
   if (!session) return new Response("Unauthorized", { status: 401 });
+  if (!process.env.MICROCMS_SERVICE_DOMAIN)
+    return new Response("MICROCMS_SERVICE_DOMAIN is required", { status: 500 });
+  if (!process.env.MICROCMS_API_KEY)
+    return new Response("MICROCMS_API_KEY is required", { status: 500 });
 
-  if (!process.env.MICROCMS_SERVICE_DOMAIN) {
-    throw new Error("MICROCMS_SERVICE_DOMAIN is required");
-  }
-
-  if (!process.env.MICROCMS_API_KEY) {
-    throw new Error("MICROCMS_API_KEY is required");
-  }
-
+  // microCMSからデータを取得
   const res = await fetch(
     `https://${process.env.MICROCMS_SERVICE_DOMAIN}.microcms.io/api/v1/photos?filters=ownerUserId[equals]${session.user?.email}&orders=-shotAt`,
     {
@@ -19,11 +16,11 @@ export async function GET() {
       headers: {
         "X-MICROCMS-API-KEY": process.env.MICROCMS_API_KEY!,
       },
-    }
+    },
   );
 
   const data = await res.json();
+  // console.log(data);
 
-  console.log(data);
   return Response.json(data);
 }
