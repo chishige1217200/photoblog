946261445ee5f67b3b54520a782f149d8568d00d
chishige1217200 <daichicchi01@gmail.com>
2026-02-10 02:55:23 +0900

    Update: API処理内容の整理

--- app/api/test/upload-photo/route.ts
+++ app/api/test/upload-photo/route.ts
@@ -3,13 +3,15 @@ import sharp from "sharp";
 
 export async function POST(req: Request) {
   const session = await auth();
-  if (!session) {
-    return new Response("Unauthorized", { status: 401 });
-  }
+  if (!session) return new Response("Unauthorized", { status: 401 });
+  if (!process.env.MICROCMS_SERVICE_DOMAIN)
+    return new Response("MICROCMS_SERVICE_DOMAIN is required", { status: 500 });
+  if (!process.env.MICROCMS_API_KEY)
+    return new Response("MICROCMS_API_KEY is required", { status: 500 });
 
+  // FormDataの読み取り
   const formData = await req.formData();
   const file = formData.get("file") as File;
-
   if (!file) {
     return new Response("No file provided", { status: 400 });
   }
@@ -19,16 +21,15 @@ export async function POST(req: Request) {
   const buffer = Buffer.from(arrayBuffer);
   const noExifBuffer = await sharp(buffer).jpeg({ quality: 90 }).toBuffer();
 
-  // ③ microCMS 送信用の FormData 作成
+  // microCMS送信用のFormDataを作成
   const microcmsForm = new FormData();
-
   microcmsForm.append(
     "file",
     new Blob([new Uint8Array(noExifBuffer)], { type: "image/jpeg" }),
-    file.name
+    file.name,
   );
 
-  // ④ microCMS メディアAPIへ送信
+  // microCMSにデータを送信
   const res = await fetch(
     `https://${process.env.MICROCMS_SERVICE_DOMAIN}.microcms-management.io/api/v1/media`,
     {
@@ -37,7 +38,7 @@ export async function POST(req: Request) {
         "X-MICROCMS-API-KEY": process.env.MICROCMS_API_KEY!,
       },
       body: microcmsForm,
-    }
+    },
   );
 
   if (!res.ok) {
@@ -46,10 +47,7 @@ export async function POST(req: Request) {
   }
 
   const data = await res.json();
-
-  // microCMS から返ってくるのはこういうデータ
-  // { url: "..." }
+  // console.log(data);
 
   return Response.json(data);
 }
-
