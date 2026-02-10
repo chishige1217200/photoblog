8cc7f2109c7b566ca3f5413ec7ee23e52205f57e
chishige1217200 <daichicchi01@gmail.com>
2026-02-10 18:30:04 +0900

    Update: cssをstylesフォルダに移動

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import "./google.css";
+import "@/styles/globals.css";
+import "@/styles/google.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
